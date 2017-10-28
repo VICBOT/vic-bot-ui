b29fbe477fdbc00e33cce53b66c936d5306d2e27
ralphchristianeclipse <ralphchristianeclipse@gmail.com>
2017-10-27 21:33:16 -0700

    Initial Commit

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  q-toolbar
+    q-toolbar-title VIC Bot
+</template>
+
+<script>
+  export default {
+    name: "Index",
+    sockets: {
+      connect() {
+        console.log("CONNECTED");
+      },
+      board(data) {
+        console.log(data);
+      },
+      ui(data) {
+        console.log(data);
+      }
+    },
+    mounted() {
+      this.$socket.emit("board", {
+        MOTOR_LEFT: 1
+      });
+    }
+  };
+</script>
+
